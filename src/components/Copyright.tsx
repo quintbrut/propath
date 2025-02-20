0a40e0113fe3899f7435593d2a25ee155ad089c9
MarkBrut <quintbrut@gmail.com>
2025-02-20 23:17:45 +0500

    remove copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,7 +4,7 @@ export const Copyright: React.FC = () => {
   return (
     <div className="flex justify-center py-2">
       <div className="px-6 py-1 text-sm text-gray-600 rounded-full bg-white/90">
-        © Nurlybek Bimakhan {new Date().getFullYear()}.
+        © {new Date().getFullYear()}
       </div>
     </div>
   );
